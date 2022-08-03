37b61f5ec08d3481183b8f59154031a482c41a29
JoeIt <josignaciot@gmail.com>
2022-08-02 21:22:24 -0300

    Changing the Detail state

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 interface ContactProps {
   contact: {
     id?: number;
@@ -7,16 +9,31 @@ interface ContactProps {
   };
 }
 
+interface ShowContactI {
+  showContact: boolean;
+}
+
 function Contact(props: ContactProps) {
+  const [showContact, setShowContact] =
+    useState<ShowContactI['showContact']>(false);
   const { contact } = props;
+
+  const onShowClick = () => {
+    setShowContact(showContact => !showContact);
+  };
 
   return (
     <div className="card card-body mb-3">
-      <h4>{contact.name}</h4>
-      <ul className="list-group">
-        <li className="list-group-item">Email: {contact.email}</li>
-        <li className="list-group-item">Phone: {contact.phone}</li>
-      </ul>
+      <h4>
+        {contact.name}{' '}
+        <i onClick={onShowClick} className="fas fa-sort-down"></i>
+      </h4>
+      {showContact && (
+        <ul className="list-group">
+          <li className="list-group-item">Email: {contact.email}</li>
+          <li className="list-group-item">Phone: {contact.phone}</li>
+        </ul>
+      )}
     </div>
   );
 }
